a3c0367dd7c09b85be26048454edc01bd213db8a
drakthephenomenal <akthephenomenal@zohomail.com>
2026-07-13 08:42:12 +0000

    Add native Android project to version control (Zoho manifest fix, custom reminder tone)

---
+++ android/app/src/main/java/app/vercel/radharadharadha/capacitor/MainActivity.java
@@ -0,0 +1,12 @@
+package app.vercel.radharadharadha.capacitor;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.bridge.getWebView().getSettings().setTextZoom(100);
+    }
+}
